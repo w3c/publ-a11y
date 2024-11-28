02e838e822469c1ea5132ddabc1cfbabebc64adc
Matt Garrish <mattgarrish@users.noreply.github.com>
2024-11-28 13:11:12 -0500

    remove old code from manifest processing

--- a11y-meta-display-guide/experiments/meta-processor/packageProcessor.js
+++ a11y-meta-display-guide/experiments/meta-processor/packageProcessor.js
@@ -683,12 +683,3 @@ var packageProcessor = (function() {
 	}
 
 })();
-
-	
-	function union(setA, setB) {
-		var _union = new Set(setA);
-		for (var elem of setB) {
-		    _union.add(elem);
-		}
-		return _union;
-	}
