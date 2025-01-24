deed5cfb61189a8430436f8d3c33b1d30b91a396
Matt Garrish <mattgarrish@users.noreply.github.com>
2025-01-24 11:15:25 -0500

    s/beforeEnd/afterEnd/

--- a11y-meta-display-guide/2.0/draft/common/js/add_ids.js
+++ a11y-meta-display-guide/2.0/draft/common/js/add_ids.js
@@ -8,6 +8,6 @@ function addStringIDs() {
 		var id_span = document.createElement('span');
 			id_span.classList.add('str-id')
 			id_span.appendChild(document.createTextNode('[ID: ' + code.id + ']'));
-		code.insertAdjacentElement('beforeEnd', id_span);
+		code.insertAdjacentElement('afterEnd', id_span);
 	});
 }
